6536fc7559bb7a52d95db4e8ccf6ebbe4c50092b
Francesco <francesco@xevolab.com>
2023-12-02 15:35:12 -0800

    Testing changes

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,7 @@
  * Author    : Francesco
  * Created at: 2023-09-24 09:53
  * Edited by : Francesco
- * Edited at : 2023-11-29 16:39
+ * Edited at : 2023-12-01 16:08
  *
  * Copyright (c) 2023 Xevolab S.R.L.
  */
@@ -25,51 +25,63 @@ describe("sign", () => {
 
 	describe("RS", () => {
 
-		const key = createPrivateKey({
-			key: fs.readFileSync("test_rsa.pem", "ascii"),
-			format: "pem",
-			type: "pkcs1"
-		});
-		const certs = parseCerts(fs.readFileSync("test_rsa.crt", "ascii"));
+		["2048", "4096"].forEach(keyLength => {
+			describe("Key length: " + keyLength, () => {
 
-		["RS256", "RS384", "RS512"].forEach(alg => {
-			describe(alg, () => {
-				const options = {
-					serialization: "compact" as const,
-					detached: false,
-
-					key,
-					alg: alg as SignAlg,
-
-					protectedHeaders: {
-						x5c: generateX5c(certs),
-						sigT: null
-					},
-				};
-
-				const token = sign(payload, options) as string;
-
-				it("should return a string", () => {
-					expect(token).to.be.a("string");
+				const key = createPrivateKey({
+					key: fs.readFileSync(`test_rsa_${keyLength}.pem`, "ascii"),
+					format: "pem",
+					type: "pkcs1"
 				});
-				it("should be a valid JWS", () => {
-					expect(token.split(".").length).to.equal(3);
-				});
-				it("should be a valid JWS with a valid header", () => {
-					expect(JSON.parse(Buffer.from(token.split(".")[0], "base64").toString("utf8"))).to.be.an("object");
+				const certs = parseCerts(fs.readFileSync(`test_rsa_${keyLength}.crt`, "ascii"));
+
+				["RS256", "RS384", "RS512"].forEach(alg => {
+					describe("Alg: " + alg, () => {
+						const options = {
+							serialization: "compact" as const,
+							detached: false,
+
+							key,
+							alg: alg as SignAlg,
+
+							protectedHeaders: {
+								x5c: generateX5c(certs),
+								sigT: null
+							},
+						};
+
+						const token = sign(payload, options) as string;
+
+						it("should return a string", () => {
+							expect(token).to.be.a("string");
+						});
+						it("should be a valid JWS", () => {
+							expect(token.split(".").length).to.equal(3);
+						});
+						it("should be a valid JWS with a valid header", () => {
+							expect(JSON.parse(Buffer.from(token.split(".")[0], "base64").toString("utf8"))).to.be.an("object");
+						});
+						it("signature lenght should be correct", () => {
+							const sigLengthHex = Buffer.from(token.split(".")[2], "base64").toString("hex").length;
+
+							// 2048 bits = 256 bytes = 512 hex chars
+							expect(sigLengthHex).to.equal(parseInt(keyLength) / 4);
+						});
+
+						// validate with jose
+						it("should be a valid JWS (JOSE)", async () => {
+							const { payload: _payload/*, protectedHeaders */ } = await jose.compactVerify(token, certs[0].publicKey);
+
+							expect(_payload.toString()).to.equal(JSON.stringify(payload));
+						});
+
+						// validate with jws
+						it("should be a valid JWS (JWS)", () => {
+							expect(jws.verify(token, alg as Algorithm, certs[0].publicKey.export({ format: "pem", type: "pkcs1" }).toString("ascii"))).to.equal(true);
+						});
+					});
 				});
 
-				// validate with jose
-				it("should be a valid JWS (JOSE)", async () => {
-					const { payload: _payload/*, protectedHeaders */ } = await jose.compactVerify(token, certs[0].publicKey);
-
-					expect(_payload.toString()).to.equal(JSON.stringify(payload));
-				});
-
-				// validate with jws
-				it("should be a valid JWS (JWS)", () => {
-					expect(jws.verify(token, alg as Algorithm, certs[0].publicKey.export({ format: "pem", type: "pkcs1" }).toString("ascii"))).to.equal(true);
-				});
 			});
 		});
 
@@ -77,55 +89,76 @@ describe("sign", () => {
 
 	describe("ES", () => {
 
-		["ES256", "ES384", "ES512"].forEach(alg => {
+		["256", "384", "521"].forEach(keyLength => {
+			describe("Key length: " + keyLength, () => {
 
-			const key = createPrivateKey({
-				key: fs.readFileSync("test_elliptic_" + alg + ".pem", "ascii"),
-				format: "pem",
-				type: "pkcs1"
-			});
-			const certs = parseCerts(fs.readFileSync("test_elliptic_" + alg + ".crt", "ascii"));
+				["ES256", "ES384", "ES512"].forEach(alg => {
 
-			describe(alg, () => {
-				let options = {
-					serialization: "compact" as const,
-					detached: false,
+					const key = createPrivateKey({
+						key: fs.readFileSync("test_elliptic_" + keyLength + ".pem", "ascii"),
+						format: "pem",
+						type: "pkcs1"
+					});
+					const certs = parseCerts(fs.readFileSync("test_elliptic_" + keyLength + ".crt", "ascii"));
 
-					key,
-					alg: alg as SignAlg,
+					describe(alg, () => {
+						let options = {
+							serialization: "compact" as const,
+							detached: false,
 
-					protectedHeaders: {
-						x5c: generateX5c(certs),
-						sigT: null
-					},
-				};
+							key,
+							alg: alg as SignAlg,
 
-				const token = sign(payload, options) as string;
+							protectedHeaders: {
+								x5c: generateX5c(certs),
+								sigT: null
+							},
+						};
 
-				it("should return a string", () => {
-					expect(token).to.be.a("string");
+						if (alg.slice(2) !== keyLength && !(alg === "ES512" && keyLength === "521")) {
+							it("should throw an error if the key is not valid for the given algorithm", () => {
+								expect(() => sign(payload, options)).to.throw(TypeError);
+							});
+
+							return
+						}
+
+						const token = sign(payload, options) as string;
+
+						it("should return a string", () => {
+							expect(token).to.be.a("string");
+						});
+						it("should be a valid JWS", () => {
+							expect(token.split(".").length).to.equal(3);
+						});
+						it("signature lenght should be correct", () => {
+							expect(Buffer.from(token.split(".")[2], "base64").toString("hex").length).to.equal({
+								"ES256": 64 * 2,
+								"ES384": 96 * 2,
+								"ES512": 132 * 2
+							}[alg]);
+						});
+						it("should be a valid JWS with a valid header", () => {
+							expect(JSON.parse(Buffer.from(token.split(".")[0], "base64").toString("utf8"))).to.be.an("object");
+						});
+
+						// validate with jose
+						it("should be a valid JWS (JOSE)", async () => {
+							const { payload: _payload/*, protectedHeaders */ } = await jose.compactVerify(token, certs[0].publicKey);
+							expect(_payload.toString()).to.equal(JSON.stringify(payload));
+						});
+
+						// validate with jws
+						it("should be a valid JWS (JWS)", () => {
+							expect(jws.verify(token, alg as Algorithm, certs[0].publicKey.export({ format: "pem", type: "spki" }).toString("ascii"))).to.equal(true);
+						});
+					});
 				});
-				it("should be a valid JWS", () => {
-					expect(token.split(".").length).to.equal(3);
-				});
-				it("should be a valid JWS with a valid header", () => {
-					expect(JSON.parse(Buffer.from(token.split(".")[0], "base64").toString("utf8"))).to.be.an("object");
-				});
 
-				// validate with jose
-				it("should be a valid JWS (JOSE)", async () => {
-					const { payload: _payload/*, protectedHeaders */ } = await jose.compactVerify(token, certs[0].publicKey);
-					expect(_payload.toString()).to.equal(JSON.stringify(payload));
-				});
-
-				// validate with jws
-				it("should be a valid JWS (JWS)", () => {
-					expect(jws.verify(token, alg as Algorithm, certs[0].publicKey.export({ format: "pem", type: "spki" }).toString("ascii"))).to.equal(true);
-				});
 			});
 		});
 
-	})
+	});
 
 });
 
